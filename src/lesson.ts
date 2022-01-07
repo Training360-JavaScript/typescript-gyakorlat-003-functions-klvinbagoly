0378e1f368d0af36dee7f1157cb7850e7aa5e804
klvinbagoly <kbalazsk1@gmail.com>
2022-01-07 07:14:36 +0100

    done

--- src/lesson.ts
+++ src/lesson.ts
@@ -19,7 +19,7 @@ export default class Calculator {
      * @param {number} b - a második szám.
      * @returns {number} a két paraméterként kapott szám összege.
      */
-
+sum(a: number, b: number){return a + b}
 
     /** 
      * 2. feladat:
@@ -29,8 +29,7 @@ export default class Calculator {
      * @param {number} b - a második szám.
      * @returns {number} a két paraméterként kapott szám különbsége.
      */
-
-    
+difference(a: number, b: number){return a - b}    
     /** 
      * 3. feladat:
      * Hozd létre a getProperty metódust.
@@ -41,6 +40,6 @@ export default class Calculator {
      * @param {User} user - az objektum.
      * @returns {any} a kulcs alapján kiolvasott tulajdonság értéke.
      */
-
+     getProperty(user: {name: string, email: string}){return user.name}
 
 }
